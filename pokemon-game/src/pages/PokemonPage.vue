de98c0a939ad39bcad58f6b14c906a0d666ff9e3
Luis Jose Padilla Benitez <luis.padilla@global-imi.com>
2024-03-19 22:25:01 -0600

    Pokemon Game: * Mostrar las opciones posibles

--- pokemon-game/src/pages/PokemonPage.vue
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -1,7 +1,7 @@
 <template>
     <h1>Who is that Pokemon?</h1>
     <PokemonPicture :pokemonId="4" :showPokemon="false" />
-    <PokemonOptions />
+    <PokemonOptions :pokemons='pokemonArray'/>
 </template>
 <script>
 import PokemonPicture from '@/components/PokemonPicture'
@@ -9,9 +9,22 @@ import PokemonOptions from '@/components/PokemonOptions'
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
 export default {
+    data() {
+        return {
+            pokemonArray: []
+        }
+    },
     components: {
         PokemonPicture,
         PokemonOptions
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArray = await getPokemonOptions()
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
     }
 }
 </script>

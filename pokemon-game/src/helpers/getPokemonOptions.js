73abbf888fd85f8358390a67f6299b5a748261d4
Luis Jose Padilla Benitez <luis.padilla@global-imi.com>
2024-03-19 22:05:52 -0600

    Pokemon Game : * Introducción a la sección * Temas puntuales de la sección * Demostración del objetivo al final de la sección * Inicio de proyecto - Pokémon Game * Estructura del proyecto y componentes * Diseño de los componentes * Funcionalidad de PokemonPicture * Lógica de los nombres de los pokémons * Obtener nombres de los 4 pokémons

---
+++ pokemon-game/src/helpers/getPokemonOptions.js
@@ -0,0 +1,39 @@
+import pokemonApi from '@/api/pokemonApi'
+
+const getPokemons = () => {
+    const pokemonsArray = Array.from(Array(650));
+    
+    return pokemonsArray.map((_, index) => index++);
+}
+
+const getPokemonNames = async ([pokemonA, pokemonB, pokemonC, pokemonD] = []) => {
+    const promisesArray = [
+        pokemonApi.get(`/${pokemonA}`),
+        pokemonApi.get(`/${pokemonB}`),
+        pokemonApi.get(`/${pokemonC}`),
+        pokemonApi.get(`/${pokemonD}`)
+    ]
+
+    const [
+        responsePokemonA,
+        responsePokemonB,
+        responsePokemonC,
+        responsePokemonD
+    ] = await Promise.all(promisesArray);
+
+    return [
+        { id: responsePokemonA.data.id, name: responsePokemonA.data.name},
+        { id: responsePokemonB.data.id, name: responsePokemonB.data.name},
+        { id: responsePokemonC.data.id, name: responsePokemonC.data.name},
+        { id: responsePokemonD.data.id, name: responsePokemonD.data.name}
+    ]
+}
+
+const getPokemonOptions = () => {
+    const mixPokemons = getPokemons().sort(() => Math.random() - 0.5)
+
+    const listPokemons = getPokemonNames(mixPokemons.splice(0, 4))
+    return listPokemons
+}
+
+export default getPokemonOptions
---
+++ pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Who is that Pokemon?</h1>
+    <PokemonPicture :pokemonId="4" :showPokemon="false" />
+    <PokemonOptions />
+</template>
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+}
+</script>
+<style>
+    
+</style>
